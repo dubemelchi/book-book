dc8c1cb6d19d4eb5edaeb7e68fb05c86b6b7f4af
dubemelchi <dubemelchi@gmail.com>
2019-10-10 06:30:16 -0400

    error handling and css for loading

---
+++ src/components/loader.js
@@ -0,0 +1,22 @@
+import { css } from '@emotion/core';
+import React from 'react';
+
+const Loader: React.FunctionComponent<{}> = ({ loading, children }) => {
+  return (
+    <>
+      {loading && (
+        <div
+          css={css`
+            color: green;
+            text-align: center;
+            padding: 20px 0;
+          `}
+        >
+          {children}
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Loader;
